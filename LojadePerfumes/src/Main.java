4f26d956ecc4b8f942a55434db869c9146e1a7d6
Aluno <Aluno@DESKTOP-LDNVJ6K>
2018-12-03 17:18:53 -0300

    fdesfr

--- LojadePerfumes/src/Main.java
+++ LojadePerfumes/src/Main.java
@@ -12,6 +12,61 @@ public class Main {
 		dao.save(p2);
 		Loja find = (Loja) dao.find(Loja.class, 22l);
 		System.out.println(find.getNome());
+		
+		
+		DAO<Perfume> dao = new DAO<Perfume>();
+		Perfume p = new Perfume();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Categiria> dao = new DAO<Categiria>();
+		Categiria p = new Categiria();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Classificacao> dao = new DAO<Classificacao>();
+		Classificacao p = new Classificacao();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Cliente> dao = new DAO<Cliente>();
+		Cliente p = new Cliente();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Entrega> dao = new DAO<Entrega>();
+		Entrega p = new Entrega();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Fragrancia> dao = new DAO<Fragrancia>();
+		Fragrancia p = new Fragrancia();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Empresa> dao = new DAO<Empresa>();
+		Empresa p = new Empresa();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Funcionario> dao = new DAO<Funcionario>();
+		Funcionario p = new Funcionario();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
+		
+		DAO<Financiamento> dao = new DAO<Financiamento>();
+		Financiamento p = new Financiamento();
+		p.setId(22l);
+		p.setNome("");
+		dao.save(p);
 	}
 	
 }
